4e28e2f5bfde01cf5f89449bfea949a1c107f1c4
Manoel Campos <manoelcampos@gmail.com>
2020-02-18 11:52:31 -0300

    Adiciona README e script para instalação de ferramentas

---
+++ ferramentas.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+sudo apt update
+sudo apt install software-properties-common apt-transport-https wget
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt update
+sudo apt install code -y
+
+sudo apt install ruby gem asciidoctor git -y
+sudo gem install asciidoctor-pdf
