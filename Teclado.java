cb73a5c6df72a89ac74018283d1258b274ce4a6d
Manoel Campos <manoelcampos@gmail.com>
2020-02-19 10:49:12 -0300

    Adiciona trechos de um arquivo com inclue

---
+++ Teclado.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Teclado{
+
+    // tag::main[]
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite seu nome: ");
+        String nome = scanner.nextLine();
+        System.out.println("Olá " + nome);
+    }
+    // tag::main[]
+}
