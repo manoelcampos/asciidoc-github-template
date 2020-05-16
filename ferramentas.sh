b0008c0f6055125b59077407892b0c84f750843d
Manoel Campos <manoelcampos@gmail.com>
2020-05-16 11:29:29 -0300

    Atualiza script para detectar se é macOS, fazer download do Homebrew e instalar ferramentas. Caso não seja, considera que é algum derivado de Debian e faz download via apt-get.
    O script não suporta outras distribuições Linux.

--- ferramentas.sh
+++ ferramentas.sh
@@ -1,10 +1,24 @@
 #!/bin/sh
-sudo apt update
-sudo apt install software-properties-common apt-transport-https wget -y
-wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-sudo apt update
-sudo apt install code -y
 
-sudo apt install make ruby gem asciidoctor git -y
-sudo gem install asciidoctor-pdf asciidoctor-revealjs
+#Se é macOS
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    brew -v || \
+    (echo "Gerenciador de pacotes homebrew não está instalado. Baixando e instalando..."; \
+    bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)")
+
+    brew install asciidoctor 
+    gem install asciidoctor-pdf asciidoctor-revealjs --user-install
+else
+    sudo apt update
+    sudo apt install software-properties-common apt-transport-https wget -y
+
+    sudo apt install make ruby gem asciidoctor git -y
+    sudo gem install asciidoctor-pdf asciidoctor-revealjs
+
+    echo "Baixando Visual Studio Code. Se a instalação não funcionar, baixe manualmente em http://code.visualstudio.com"
+    wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+    sudo apt update
+    sudo apt install code -y
+fi
+
