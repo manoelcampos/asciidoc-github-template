31f731db2457a0a6df82936165c4acb84c1b1391
Manoel Campos <manoelcampos@gmail.com>
2020-08-14 18:14:16 -0300

    Alterar apt por apt-get no ferramentas.sh
    O apt-get é mais apropriado para ser usado em scripts,
    evitando o warning "Unstable CLI Interface".

--- ferramentas.sh
+++ ferramentas.sh
@@ -9,16 +9,16 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     brew install asciidoctor 
     gem install asciidoctor-pdf asciidoctor-revealjs --user-install
 else
-    sudo apt update
-    sudo apt install software-properties-common apt-transport-https wget -y
+    sudo apt-get update
+    sudo apt-get install software-properties-common apt-transport-https wget -y
 
-    sudo apt install make ruby gem asciidoctor git -y
+    sudo apt-get install make ruby gem asciidoctor git -y
     sudo gem install asciidoctor-pdf asciidoctor-revealjs
 
     echo "Baixando Visual Studio Code. Se a instalação não funcionar, baixe manualmente em http://code.visualstudio.com"
     wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
     sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-    sudo apt update
-    sudo apt install code -y
+    sudo apt-get update
+    sudo apt-get install code -y
 fi
 
